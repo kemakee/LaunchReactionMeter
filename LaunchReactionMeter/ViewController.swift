45f9dcb961cde6532e693fa1d222ac14cddc61b4
Ákos Kemenes <kemakee@gmail.com>
2018-02-26 20:13:01 +0100

    Initial Commit

---
+++ LaunchReactionMeter/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LaunchReactionMeter
+//
+//  Created by Ákos Kemenes on 2018. 02. 26..
+//  Copyright © 2018. Ákos Kemenes. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
