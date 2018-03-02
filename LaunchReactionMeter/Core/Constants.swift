f64046c751d16ba15a32a27a2568538967231a1f
Ákos Kemenes <kemakee@gmail.com>
2018-03-02 21:29:10 +0100

    first commit

---
+++ LaunchReactionMeter/Core/Constants.swift
@@ -0,0 +1,9 @@
+//
+//  Constants.swift
+//  LaunchReactionMeter
+//
+//  Created by Ákos Kemenes on 2018. 02. 26..
+//  Copyright © 2018. Ákos Kemenes. All rights reserved.
+//
+
+import Foundation
